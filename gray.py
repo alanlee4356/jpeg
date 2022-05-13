c0173ac71a60e296c138242b329787796e9c3c15
이성현 <alanlee4356@gmail.com>
2022-05-13 12:48:19 +0900

    no message

--- gray.py
+++
@@ -1,8 +0,0 @@
-import cv2
-img = cv2.imread('/Users/alanlee/Downloads/jpeg-python-master/lena.bmp',cv2.IMREAD_GRAYSCALE)
-
-thresh = 128
-
-img_binary = cv2.threshold(img,thresh,255,cv2.THRESH_BINARY)[1]
-
-cv2.imwrite('/Users/alanlee/Downloads/jpeg-python-master/lena_gray.bmp',img_binary)
